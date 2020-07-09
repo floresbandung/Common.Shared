7277056a530a82da41ae549a1b4aac42c8c0a89b
kunkun kurniawan <kurnia.wirawan@gmail.com>
2020-07-09 22:23:53 +0700

    add shared static message

---
+++ Fault/StaticMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DD.TataBuku.Shared.Fault
+{
+    public static class StaticMessage
+    {
+        public const string INVALID_CONNECTION_STRING = "Connection to database server failed";
+    }
+}
