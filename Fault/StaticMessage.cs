f51e762a7fbdd314ec4f85d6c922576d534cad4f
kunkun kurniawan <kurnia.wirawan@gmail.com>
2020-07-18 20:25:53 +0700

    add new message

--- Fault/StaticMessage.cs
+++ Fault/StaticMessage.cs
@@ -7,7 +7,11 @@ namespace DD.TataBuku.Shared.Fault
 {
     public static class StaticMessage
     {
+        public const string PROCESS_ACTIVITY_NOT_FOUND = "Process activity not found";
+        public const string INVALID_PROCESS_ACTIVITY_INDEX = "Invalid process activity index";
         public const string INVALID_CONNECTION_STRING = "Connection to database server failed";
         public const string DEFAULT_ERROR_MESSAGE = "Something went wrong please try again later";
+        public const string INVALID_USER_RIGHT_APPROVAL = "Current user does not have right no create approval";
+        public const string INVALID_REQUEST_TYPE = "Invalid request type";
     }
 }
